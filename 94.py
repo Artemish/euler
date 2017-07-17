04f4e209d813a6ffda93147b36393bfdea57befa
Artemish <mitch.cro@gmail.com>
2017-07-17 16:51:55 -0500

    #94 - Almost Equilateral Triangles

--- 94.py
+++ 94.py
@@ -1,25 +1,29 @@
-from utils import pyth_triple_gen
+from utils import isqrt, square_root_convergents
 
-TOP = 1000000
-
-def isqrt(n):
-    x = n
-    y = (x + 1) // 2
-    while y < x:
-        x = y
-        y = (x + n // x) // 2
-    return x
+TOP = 1000000000
 
 total = 0
 
-for x in range(2, TOP/6+2):
-    if (x % 100000) == 0:
-        print(x)
+for (c,d) in square_root_convergents(3):
+    if (c * c - 3 * d * d == 1):
+        if (c % 3) == 1:
+            k = (c + 2) // 3
+        elif (c % 3) == 2:
+            k = (c - 2) // 3
+        else:
+            continue
 
-    n = x * (3 * x + 2)
-    s = isqrt(n)
-    if s*s == n:
-        print(x, 6*x + 4)
-        total += 6*x + 4
+        print("+/- {} {}".format(6*k+2, 6*k-2))
+
+        if (6 * k - 2) > TOP:
+            break
+
+        inner = 3*k*k - 4*k + 1
+        if isqrt(inner) ** 2 == inner:
+            total += (6*k-2)
+
+        inner = 3*k*k + 4*k + 1
+        if isqrt(inner) ** 2 == inner:
+            total += (6*k+2)
 
 print(total)
