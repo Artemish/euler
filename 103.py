5e795669be971b20efbd70abb6a906c00946e1b1
Artemish <mitch.cro@gmail.com>
2017-10-13 15:14:18 -0500

    103: special subset sums

--- 103.py
+++ 103.py
@@ -1,12 +1,24 @@
-def n_sets_of_sum(n, total, top=True):
-    if n == 0:
-        yield set()
-        return
+from utils import powerset
+from itertools import combinations
 
-    minimum = 1
-    maximum = (total / n) - sum(range(n))
-    for new_element in range(minimum, maximum + 1, top=False):
-        for remaining in n_sets_of_sum(n - 1, total - new_element):
-            yield set([new_element]) | remaining
+def test(s):
+    all_subsets = powerset(s)
 
+    for (s1, s2) in combinations(all_subsets, 2):
+        if s1 == s2:
+            continue
 
+        if len(s1) > len(s2) and sum(s1) <= sum(s2):
+            return False
+        elif len(s2) > len(s1) and sum(s2) <= sum(s1):
+            return False
+        elif sum(s1) == sum(s2):
+            return False
+
+    return True
+
+for c in combinations(xrange(20, 45), 7):
+    s = set(c)
+    if test(s):
+        print(''.join(map(str, c)))
+        break
