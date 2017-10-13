f9450e1ba19593f3e6d0728ab2570caff8d8d41a
Artemish <mitch.cro@gmail.com>
2017-10-13 15:16:43 -0500

    243: Resilient numbers

--- 243.py
+++ 243.py
@@ -1,12 +1,44 @@
-from utils import phi_field_direct
+from fractions import Fraction
+from itertools import combinations_with_replacement
+from utils import primes_below
 
-TOP = 100000000
+TOP = 30
 
-phis = phi_field_direct(TOP)
+target = Fraction(15499, 94744)
 
-print("Got phi field")
+def main(primes, p_i, n, resilience, depth=1):
+    if depth > 10:
+        return []
 
-for i in range(2, TOP):
-    if (phis[i] * 94744) < ((i - 1) * 15499):
-        print(str(i))
-        break
+    global target 
+
+    p = primes[p_i]
+
+    prime_loss = Fraction((p-1)*(n-1), n*p - 1)
+    two_loss = Fraction(2*n-2, 2*n-1)
+
+    prime_branch_resilience = resilience * prime_loss
+    prime_n = n * p
+
+    two_branch_resilience = resilience * two_loss
+    two_n = 2 * n
+
+    ret = []
+
+    if prime_branch_resilience < target:
+        ret = [prime_n]
+    if two_branch_resilience < target:
+        ret.append(two_n)
+
+    if len(ret) != 0:
+        return ret
+
+    print("Taking prime branch with {}, {}".format(n, primes[p_i]))
+    prime_branch_found = main(primes, p_i + 1, prime_n, prime_branch_resilience, depth+1)
+    print("Taking twos branch with {}".format(n))
+    two_branch_found = main(primes, p_i, two_n, two_branch_resilience, depth+1)
+    return prime_branch_found + two_branch_found
+
+primes = primes_below(TOP)
+candidates = main(primes, 1, 2, Fraction(1))
+print(min(candidates))
