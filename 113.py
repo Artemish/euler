127f9a7ebd0986b628a3f3920804ae481f9b7ceb
Artemish <mitch.cro@gmail.com>
2018-05-30 17:48:15 -0500

    Fixed 113

--- 113.py
+++ 113.py
@@ -1,14 +1,29 @@
-TOP = 6
+from collections import Counter
 
-def recurrence(l, r):
-    if l == 1:
-        return 1
-    if r == 0:
-        return 1
+DIGITS = 100
 
-    return sum([recurrence(i+1, r-1) for i in range(l)])
+incmap = {(1, i) : 1 for i in range(1,10)}
+for x in range(2,DIGITS+1):
+    for d in range(1,10):
+        incmap[(x,d)] = 0
 
-inc = recurrence(10, TOP)
-dec = recurrence(10, TOP)
+    for d in range(1,10):
+        val = incmap[(x-1,d)]
+        for higher in range(d, 10):
+            incmap[(x,higher)] += val
 
-print(inc + dec)
+decmap = {(1, i) : 1 for i in range(1,10)}
+decmap[(1,0)] = 0
+
+for x in range(2,DIGITS+1):
+    for d in range(10):
+        decmap[(x,d)] = 0
+
+    for d in range(10):
+        val = decmap[(x-1,d)]
+        for lower in range(d+1):
+            decmap[(x,lower)] += val
+
+allequal = 9 * (DIGITS)
+
+print(sum(incmap.values()) + sum(decmap.values()) - allequal)
